1143b28592eb1aaaa7e7eef162a9bf85b2897a2a
GitaMansouri <gita.mansouri.84@gmail.com>
2025-04-18 11:43:44 -0700

    build music class

--- classes/Music.java
+++ classes/Music.java
@@ -1,4 +1,44 @@
 package classes;
 
+import java.util.*;
+
 public class Music {
+    private String title;
+    private User singer;
+    public int numberOfStream = 0;
+    private static ArrayList<Music> allMusics = new ArrayList<>();
+
+    public Music(String title, User singer){
+        this.title = title;
+        this.singer = singer;
+        allMusics.add(this);
+    }
+
+    public void play(){
+        System.out.println("Muisc '" + title + "' by " + singer.getUsername() + "is playing");
+    }
+
+    public ArrayList<Music> search(String title) {
+        ArrayList<Music> foundMusics = new ArrayList<>();
+        for (Music music : allMusics) {
+            if (music.title.equalsIgnoreCase(title)) {
+                foundMusics.add(music);
+            }
+        }
+        if (foundMusics.isEmpty()){
+            return null;
+        }
+        else {
+            return foundMusics;
+        }
+    }
+
+    public Music search(String title, String singerName) {
+        for (Music music : allMusics) {
+            if (music.title.equalsIgnoreCase(title) && music.singer.getUsername().equalsIgnoreCase(singerName)) {
+                return music;
+            }
+        }
+        return null;
+    }
 }
